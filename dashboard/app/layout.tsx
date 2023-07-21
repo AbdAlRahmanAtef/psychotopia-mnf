8cd285998fcfcc776eeee98f775dd0433f61a89d
Abdalrahman Atef <abdalrhman.atef.ramadan@gmail.com>
2023-07-21 18:16:25 +0300

    moified

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -6,14 +6,9 @@ import { Metadata } from 'next';
 import './globals.css';
 import Layout from '@/components/Layout';
 
-export const metadata: Metadata = {
-  title: 'Dashboard',
-};
-
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <head></head>
       <body>
         <Provider store={store}>
           <Layout>{children}</Layout>
