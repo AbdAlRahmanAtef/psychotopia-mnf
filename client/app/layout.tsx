d119d94ae8d64d8f1a233dff1b08339c198d39f7
Abdalrahman Atef <abdalrhman.atef.ramadan@gmail.com>
2023-07-21 18:11:56 +0300

    modified

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,14 +4,8 @@ import './globals.css';
 import Header from '@/components/Header';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from 'next-themes';
-import { Metadata } from 'next';
 import ThemeContextProvider from '@/context/ThemeContext';
 import Footer from '@/components/Footer';
-
-export const metadata: Metadata = {
-  title: 'Home',
-  description: 'Welcome to Next.js',
-};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
